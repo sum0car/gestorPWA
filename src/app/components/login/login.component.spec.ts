6c4250b69773748b47b299e1d1c590955cb144ce
karli <karlitos.braga08@gmail.com>
2024-11-08 23:17:02 -0300

    add login screen WIP

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder } from '@angular/forms';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  public loginForm!: FormGroup;
+
+  public constructor(private fb: FormBuilder) {
+    this.loginForm = fb.group({
+      user: ['', Validators.required, Validators.email],
+      password: ['', Validators.required]
+    });
+  }
+
+  
 }
